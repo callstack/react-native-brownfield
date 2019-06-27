7175c867f2290cddd8efa5df3fe177af7d829ac2
michalchudziak <michal.chudziak@gmail.com>
2019-06-27 22:24:45 +0200

    improve example

--- example/native/index.js
+++ example/native/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {AppRegistry, StyleSheet, Text, View, NativeModules, Button, Image} from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import {createStackNavigator, createAppContainer, NavigationEvents} from 'react-navigation';
 
 const getRandomTheme = () => {
   const getRandomValue = () => Math.round(Math.random() * 255);
@@ -21,33 +21,42 @@ class HomeScreen extends React.Component {
 
   render() {
     const colors = this.props.navigation.getParam('theme', getRandomTheme());
+    const isFirstRoute = this.props.navigation.isFirstRouteInParent();
 
     return (
-      <View style={[styles.container, {backgroundColor: colors.primary}]}>
-        <Text style={[styles.text, {color: colors.secondary}]}>React Native Screen</Text>
-
-        <Button
-          onPress={() => { NativeModules.ReactNativeBrownfield.setPopGestureRecognizer(false) }}
-          color={colors.secondary}
-          title="Disable back gesture"
-        />
-
-        <Button
-          onPress={() => { NativeModules.ReactNativeBrownfield.popToNative(true) }}
-          color={colors.secondary}
-          title="Pop to native"
-        />
-
-        <Button
-          onPress={() => {
-            this.props.navigation.push('Home', {
-              theme: getRandomTheme(),
-            });
+      <>
+        <NavigationEvents
+          onWillFocus={() => {
+            const isFirstRoute = this.props.navigation.isFirstRouteInParent();
+            NativeModules.ReactNativeBrownfield.setPopGestureRecognizer(isFirstRoute);
           }}
-          color={colors.secondary}
-          title="Push next screen"
         />
-      </View>
+        <View style={[styles.container, {backgroundColor: colors.primary}]}>
+          <Text style={[styles.text, {color: colors.secondary}]}>React Native Screen</Text>
+
+          <Button
+            onPress={() => {
+              this.props.navigation.push('Home', {
+                theme: getRandomTheme(),
+              });
+            }}
+            color={colors.secondary}
+            title="Push next screen"
+          />
+
+          <Button
+            onPress={() => { 
+              if (isFirstRoute) {
+                NativeModules.ReactNativeBrownfield.popToNative(true); 
+              } else {
+                this.props.navigation.goBack();
+              }      
+            }}
+            color={colors.secondary}
+            title="Go back"
+          />
+        </View>
+      </>
     );
   }
 }
