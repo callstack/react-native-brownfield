c8de52d1ae9dc8585ec72448b849ced4c814f5aa
michalchudziak <michal.chudziak@gmail.com>
2019-07-03 11:38:41 +0200

    working java example

--- android/src/main/java/com/callstack/reactnativebrownfield/BridgeManager.kt
+++ android/src/main/java/com/callstack/reactnativebrownfield/BridgeManager.kt
@@ -1,5 +1,37 @@
 package com.callstack.reactnativebrownfield
 
-class BridgeManager {
-  val test = "test"
+import android.app.Application
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.ReactContext
+import java.util.concurrent.atomic.AtomicBoolean
+
+class BridgeManager private constructor(val rnHost: ReactNativeHost) {
+  companion object {
+    private lateinit var instance: BridgeManager
+    private val initialized = AtomicBoolean()
+
+    val shared: BridgeManager get() = instance
+
+    fun initialize(rnHost: ReactNativeHost) {
+      if(initialized.getAndSet(true)) {
+        instance = BridgeManager(rnHost)
+      }
+    }
+  }
+
+  private var reactInstanceManager: ReactInstanceManager? = null
+
+  fun startReactNative(listener: ((initialized: Boolean) -> Unit)?) {
+    reactInstanceManager = rnHost.reactInstanceManager
+
+    if (listener != null) {
+      reactInstanceManager?.addReactInstanceEventListener(object: ReactInstanceManager.ReactInstanceEventListener {
+        override fun onReactContextInitialized(context: ReactContext?) {
+          listener(true)
+        }
+      })
+    }
+  }
 }
---
+++ example/native/android/app/src/main/java/com/callstack/nativeexample/MainApplication.java
@@ -0,0 +1,41 @@
+package com.callstack.nativeexample;
+
+import android.app.Application;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.callstack.reactnativebrownfield.BridgeManager;
+
+import java.util.List;
+
+public class MainApplication extends Application {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        BridgeManager.Companion.initialize(mReactNativeHost);
+        SoLoader.init(this, /* native exopackage */ false);
+    }
+}
