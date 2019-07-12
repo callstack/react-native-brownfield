def6e01e80bcbb06279d3914dfed60f047f455d6
Dratwas <drapich.piotr@gmail.com>
2019-07-12 16:42:41 +0200

    fix autolinking android

--- android/src/main/java/com/callstack/reactnativebrownfield/ReactNativeBrownfieldPackage.kt
+++ android/src/main/java/com/callstack/reactnativebrownfield/ReactNativeBrownfieldPackage.kt
@@ -9,7 +9,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 import com.facebook.react.uimanager.ReactShadowNode
 
-class ReactNativeBrownfieldPackage: ReactPackage {
+class ReactNativeBrownfieldPackage : ReactPackage {
     override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
         return Collections.emptyList()
     }
