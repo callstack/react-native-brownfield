279c6e9586ae8c09cbe09881c8e21aea59a89fee
Mariia Marchenkova <marchewka@iPhoneJkLankauf.localdomain>
2020-01-14 12:47:00 +0100

    chore: updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+    'module:metro-react-native-babel-preset',
+  ],
 };
