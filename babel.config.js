6e7ba7239899b256d9ad2452252ef9426ab4bc7d
michalchudziak <michal.chudziak@gmail.com>
2020-01-15 23:23:27 +0100

    Finish TS impl

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-    'module:metro-react-native-babel-preset',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
