2a4e141019b7482f96d6f8b5de331703272f7656
michalchudziak <michal.chudziak@gmail.com>
2020-01-16 09:49:17 +0100

    Finish dev setup

--- examples/bundler/metro.config.js
+++ examples/bundler/metro.config.js
@@ -2,4 +2,9 @@ const path = require('path');
 
 module.exports = {
   projectRoot: path.resolve('.'),
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../packages/bridge'),
+    path.resolve(__dirname, '../../packages/cli'),
+  ],
 };
