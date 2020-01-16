c7f5d90b3facc2d97b9c9378e1830fa8d2181d5e
Mariia Marchenkova <marchewka@iPhoneJkLankauf.localdomain>
2020-01-16 14:11:09 +0100

    feat: copy files on build-ios command

--- packages/cli/src/commands/build-ios.ts
+++ packages/cli/src/commands/build-ios.ts
@@ -1,19 +1,27 @@
 import fs from 'fs';
-// import path from 'path';
+import path from 'path';
 import {execSync} from 'child_process';
+import copyFiles from '../tools/copyFiles';
 import {BuildArtifact} from '../types';
 
-export default function buildArtifact({entryFile}: BuildArtifact) {
+export default async function buildArtifact({entryFile}: BuildArtifact) {
   const buildDir = `${process.cwd()}/build/brownfield`;
+  const libraryPath = path.join(
+    path.dirname(require.resolve('@react-native-brownfield/cli')),
+    '..',
+    'ios',
+  );
 
   if (!fs.existsSync(buildDir)) {
     fs.mkdirSync(buildDir, {recursive: true});
   }
 
-  // const libraryPath = path.join(
-  //   path.dirname(require.resolve('@react-native-brownfield/cli')),
-  //   '..',
-  // );
+  try {
+    await copyFiles(libraryPath, `${buildDir}/ios`);
+  } catch (e) {
+    console.log(e);
+    return;
+  }
 
   try {
     const result = execSync(
