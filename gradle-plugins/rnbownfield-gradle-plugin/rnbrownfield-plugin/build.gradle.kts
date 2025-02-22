61fa3b373a00ed14c71d12393c400551b612926b
hurali97 <hurali97@gmail.com>
2025-02-22 14:22:21 +0500

    chore: configure ktlint and detekt

--- gradle-plugins/rnbownfield-gradle-plugin/rnbrownfield-plugin/build.gradle.kts
+++ gradle-plugins/rnbownfield-gradle-plugin/rnbrownfield-plugin/build.gradle.kts
@@ -1,6 +1,28 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
     `java-gradle-plugin`
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+ktlint {
+    debug.set(false)
+    verbose.set(true)
+    android.set(false)
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+    enableExperimentalRules.set(true)
+
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
 }
 
 group = property("GROUP").toString()
@@ -18,4 +40,9 @@ gradlePlugin {
 repositories {
     mavenCentral()
     google()
+}
+
+tasks.register("lint") {
+    dependsOn(":ktlintFormat")
+    dependsOn(":detekt")
 }
