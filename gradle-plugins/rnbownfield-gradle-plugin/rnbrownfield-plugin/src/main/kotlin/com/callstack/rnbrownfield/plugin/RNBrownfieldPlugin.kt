ea4b9e4b230c23ffa9ed7ae48f2087ab9f04e876
hurali97 <hurali97@gmail.com>
2025-02-23 20:01:28 +0500

    feat: update BrownfieldPlugin

--- gradle-plugins/rnbownfield-gradle-plugin/rnbrownfield-plugin/src/main/kotlin/com/callstack/rnbrownfield/plugin/RNBrownfieldPlugin.kt
+++ gradle-plugins/rnbownfield-gradle-plugin/rnbrownfield-plugin/src/main/kotlin/com/callstack/rnbrownfield/plugin/RNBrownfieldPlugin.kt
@@ -1,50 +1,80 @@
 package com.callstack.rnbrownfield.plugin
 
+import com.callstack.rnbrownfield.artifacts.ArtifactsResolver
+import com.callstack.rnbrownfield.processors.VariantPackagesProperty
+import com.callstack.rnbrownfield.shared.BaseProject
 import com.callstack.rnbrownfield.shared.Constants.PROJECT_ID
 import com.callstack.rnbrownfield.shared.Logging
-import com.callstack.rnbrownfield.utils.AndroidArchiveLibrary
 import com.callstack.rnbrownfield.utils.DirectoryManager
 import com.callstack.rnbrownfield.utils.Extension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
-import org.gradle.api.provider.MapProperty
+import org.gradle.api.internal.file.FileResolver
+import org.gradle.api.internal.tasks.TaskDependencyFactory
+import org.gradle.internal.model.CalculatedValueContainerFactory
+import javax.inject.Inject
 
-class RNBrownfieldPlugin : Plugin<Project> {
-    private lateinit var project: Project
-    private lateinit var extension: Extension
-    private lateinit var projectConfigurations: ProjectConfigurations
-    private lateinit var variantPackagesProperty: MapProperty<String, List<AndroidArchiveLibrary>>
+class RNBrownfieldPlugin
+    @Inject
+    constructor(
+        private val calculatedValueContainerFactory: CalculatedValueContainerFactory,
+        private val taskDependencyFactory: TaskDependencyFactory,
+        private val fileResolver: FileResolver,
+    ) : Plugin<Project> {
+        private lateinit var project: Project
+        private lateinit var extension: Extension
+        private lateinit var projectConfigurations: ProjectConfigurations
 
-    override fun apply(project: Project) {
-        println("Hello from RNBrownfieldPlugin!")
-        this.project = project
-        verifyAndroidPluginApplied()
-        Logging.project = project
-        DirectoryManager.project = project
-        this.extension = project.extensions.create(Extension.NAME, Extension::class.java)
-        projectConfigurations = ProjectConfigurations(project)
-        projectConfigurations.setup()
-        registerRClassTransformer()
-    }
+        override fun apply(project: Project) {
+            println("Hello from RNBrownfieldPlugin!")
+            verifyAndroidPluginApplied(project)
 
-    /**
-     * Verifies and throws error if `com.android.library` plugin is not applied
-     */
-    private fun verifyAndroidPluginApplied() {
-        if (!project.plugins.hasPlugin("com.android.library")) {
-            throw ProjectConfigurationException(
-                "$PROJECT_ID must be applied to an android library project",
-                Throwable("Apply $PROJECT_ID"),
-            )
+            initializers(project)
+            projectConfigurations.setup()
+            registerRClassTransformer()
+
+            project.afterEvaluate {
+                afterEvaluate()
+            }
+        }
+
+        private fun initializers(project: Project) {
+            this.project = project
+            Logging.project = project
+            DirectoryManager.project = project
+            RClassTransformer.project = project
+            this.extension = project.extensions.create(Extension.NAME, Extension::class.java)
+            projectConfigurations = ProjectConfigurations(project)
+            VariantPackagesProperty.setVariantPackagesProperty(project)
+        }
+
+        /**
+         * Verifies and throws error if `com.android.library` plugin is not applied
+         */
+        private fun verifyAndroidPluginApplied(project: Project) {
+            if (!project.plugins.hasPlugin("com.android.library")) {
+                throw ProjectConfigurationException(
+                    "$PROJECT_ID must be applied to an android library project",
+                    Throwable("Apply $PROJECT_ID"),
+                )
+            }
+        }
+
+        /**
+         * Transforms RClass
+         */
+        private fun registerRClassTransformer() {
+            RClassTransformer.registerASMTransformation()
+        }
+
+        private fun afterEvaluate() {
+            val baseProject = BaseProject()
+            baseProject.project = project
+            val artifactsResolver = ArtifactsResolver(projectConfigurations.getConfigurations(), baseProject, extension)
+            artifactsResolver.calculatedValueContainerFactory = calculatedValueContainerFactory
+            artifactsResolver.taskDependencyFactory = taskDependencyFactory
+            artifactsResolver.fileResolver = fileResolver
+            artifactsResolver.processArtifacts()
         }
     }
-
-    /**
-     * Transforms RClass
-     */
-    private fun registerRClassTransformer() {
-        variantPackagesProperty = project.objects.mapProperty(String::class.java, List::class.java as Class<List<AndroidArchiveLibrary>>)
-        RClassTransformer.registerASMTransformation(project, variantPackagesProperty)
-    }
-}
