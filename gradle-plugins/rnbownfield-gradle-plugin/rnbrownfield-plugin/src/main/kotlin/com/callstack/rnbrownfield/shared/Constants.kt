0d929a09734a6f9348f7506344e09df1a25065a8
hurali97 <hurali97@gmail.com>
2025-02-23 20:01:04 +0500

    feat: shorten project ID

--- gradle-plugins/rnbownfield-gradle-plugin/rnbrownfield-plugin/src/main/kotlin/com/callstack/rnbrownfield/shared/Constants.kt
+++ gradle-plugins/rnbownfield-gradle-plugin/rnbrownfield-plugin/src/main/kotlin/com/callstack/rnbrownfield/shared/Constants.kt
@@ -1,8 +1,8 @@
 package com.callstack.rnbrownfield.shared
 
 object Constants {
-    const val PROJECT_ID = "com.callstack.rnbrownfield-gradle-plugin"
-    const val PLUGIN_NAME = "rnbrownfield-gradle-plugin"
+    const val PROJECT_ID = "com.callstack.rnbrownfield-plugin"
+    const val PLUGIN_NAME = "rnbrownfield-plugin"
 
     const val RE_BUNDLE_FOLDER = "aar_rebundle"
     const val INTERMEDIATES_TEMP_DIR = PLUGIN_NAME
