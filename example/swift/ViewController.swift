c5ce25dbd4e22540663c6672a22876693bfec9c5
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-03-07 18:00:18 +0100

    feat: SwiftUI support

--- example/swift/ViewController.swift
+++ example/swift/ViewController.swift
@@ -1,24 +1,16 @@
-//
-//  ViewController.swift
-//  SwiftExample
-//
-//  Created by Michal Chudziak on 25/08/2019.
-//  Copyright © 2019 Callstack. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
-    @IBAction func openReactNative(_ sender: UIButton) {
-        self.navigationController?.pushViewController(
-            ReactNativeViewController(moduleName: "ReactNative"),
-            animated: true
-        )
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  @IBAction func openReactNative(_ sender: UIButton) {
+    self.navigationController?.pushViewController(
+      ReactNativeViewController(moduleName: "ReactNative"),
+      animated: true
+    )
+  }
 }
 
