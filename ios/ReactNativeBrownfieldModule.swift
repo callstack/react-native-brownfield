0ca5f66f05a66f88d14e3def19e685e42b23aef5
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-03-19 09:58:30 +0100

    feat: SwiftUI support (#93)
    * feat: SwiftUI support
    
    * feat: better code documentation

--- ios/ReactNativeBrownfieldModule.swift
+++ ios/ReactNativeBrownfieldModule.swift
@@ -4,11 +4,15 @@ import React
 public class ReactNativeBrownfieldModuleImpl: NSObject {
   static public func setPopGestureRecognizerEnabled(_ enabled: Bool) {
     let userInfo = ["enabled": enabled]
-    NotificationCenter.default.post(name: Notification.Name.togglePopGestureRecognizer, object: nil, userInfo: userInfo)
+    DispatchQueue.main.async {
+      NotificationCenter.default.post(name: Notification.Name.togglePopGestureRecognizer, object: nil, userInfo: userInfo)
+    }
   }
   
   static public func popToNative(animated: Bool) {
     let userInfo = ["animated": animated]
-    NotificationCenter.default.post(name: Notification.Name.popToNative, object: nil, userInfo: userInfo)
+    DispatchQueue.main.async {
+      NotificationCenter.default.post(name: Notification.Name.popToNative, object: nil, userInfo: userInfo)
+    }
   }
 }
