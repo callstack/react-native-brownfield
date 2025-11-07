e9b5622abbd6d1a303f65f0e244599cca01979bc
artus9033 <artus9033@gmail.com>
2025-11-07 10:02:53 +0100

    chore: format & cleanup files

--- android/src/main/java/com/callstack/reactnativebrownfield/ReactNativeBrownfieldPackage.kt
+++ android/src/main/java/com/callstack/reactnativebrownfield/ReactNativeBrownfieldPackage.kt
@@ -1,23 +1,22 @@
 package com.callstack.reactnativebrownfield
 
 import android.view.View
-import java.util.Collections
-
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ViewManager
 import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+import java.util.Collections
 
 
 class ReactNativeBrownfieldPackage : ReactPackage {
-  override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
-    return Collections.emptyList()
-  }
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
+        return Collections.emptyList()
+    }
 
-  override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
-    val modules = ArrayList<NativeModule>()
-    modules.add(ReactNativeBrownfieldModule(reactContext))
-    return modules
-  }
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+        val modules = ArrayList<NativeModule>()
+        modules.add(ReactNativeBrownfieldModule(reactContext))
+        return modules
+    }
 }
--- android/src/newarch/ReactNativeBrownfieldModule.kt
+++ android/src/newarch/ReactNativeBrownfieldModule.kt
@@ -4,34 +4,34 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
 
 class ReactNativeBrownfieldModule(reactContext: ReactApplicationContext) :
-  NativeReactNativeBrownfieldModuleSpec(reactContext) {
-  companion object {
-    var shouldPopToNative: Boolean = false
-  }
-
-  @ReactMethod
-  override fun popToNative(animated: Boolean) {
-    shouldPopToNative = true
-    onBackPressed()
-  }
-
-  @ReactMethod
-  override fun setPopGestureRecognizerEnabled(enabled: Boolean) {
-    shouldPopToNative = enabled
-  }
-
-  @ReactMethod
-  override fun setHardwareBackButtonEnabled(enabled: Boolean) {
-    shouldPopToNative = enabled
-  }
-
-  private fun onBackPressed() {
-    reactApplicationContext.currentActivity?.runOnUiThread {
-      reactApplicationContext.currentActivity?.onBackPressed()
+    NativeReactNativeBrownfieldModuleSpec(reactContext) {
+    companion object {
+        var shouldPopToNative: Boolean = false
     }
-  }
 
-  override fun getName(): String {
-    return "ReactNativeBrownfield"
-  }
+    @ReactMethod
+    override fun popToNative(animated: Boolean) {
+        shouldPopToNative = true
+        onBackPressed()
+    }
+
+    @ReactMethod
+    override fun setPopGestureRecognizerEnabled(enabled: Boolean) {
+        shouldPopToNative = enabled
+    }
+
+    @ReactMethod
+    override fun setHardwareBackButtonEnabled(enabled: Boolean) {
+        shouldPopToNative = enabled
+    }
+
+    private fun onBackPressed() {
+        reactApplicationContext.currentActivity?.runOnUiThread {
+            reactApplicationContext.currentActivity?.onBackPressed()
+        }
+    }
+
+    override fun getName(): String {
+        return "ReactNativeBrownfield"
+    }
 }
