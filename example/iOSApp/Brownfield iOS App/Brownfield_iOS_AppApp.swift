c14f9d7ec9b2749db0fcd678382887802c30b54c
artus9033 <artus9033@gmail.com>
2025-12-12 08:23:43 +0100

    chore: bootstrap demo iOS app

---
+++ example/iOSApp/Brownfield iOS App/Brownfield_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brownfield_iOS_AppApp.swift
+//  Brownfield iOS App
+//
+//  Created by Artur Morys-Magiera on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Brownfield_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ example/iOSApp/Brownfield iOS App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brownfield iOS App
+//
+//  Created by Artur Morys-Magiera on 12/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
