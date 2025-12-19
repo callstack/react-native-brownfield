6d3875e41b0a3377eeeed450b23130a9245d90c5
artus9033 <artus9033@gmail.com>
2025-12-19 17:00:55 +0100

    feat: integrate brownfield with demo iOS app

--- example/iOSApp/Brownfield iOS App/ContentView.swift
+++ example/iOSApp/Brownfield iOS App/ContentView.swift
@@ -1,21 +1,56 @@
-//
-//  ContentView.swift
-//  Brownfield iOS App
-//
-//  Created by Artur Morys-Magiera on 12/12/2025.
-//
-
+import ReactBrownfield
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            MainScreen()
+                .padding(16)
         }
+    }
+}
+
+struct MainScreen: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            GreetingCard(name: "iOS")
+
+            ReactNativeView(moduleName: "RNApp")
+                .navigationBarHidden(true)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .background(Color(UIColor.systemBackground))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
+    }
+}
+
+struct GreetingCard: View {
+    let name: String
+    @State private var counter: Int = 0
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Hello native \(name) 👋")
+                .font(.title3)
+                .multilineTextAlignment(.center)
+
+            Text(
+                "You clicked the button \(counter) time\(counter == 1 ? "" : "s")"
+            )
+            .multilineTextAlignment(.center)
+            .font(.body)
+
+            Button("Increment counter") {
+                counter += 1
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity)
+        .background(Color(UIColor.secondarySystemBackground))
+        .cornerRadius(16)
+        .shadow(radius: 4)
     }
 }
 
