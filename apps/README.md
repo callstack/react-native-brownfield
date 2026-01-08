d47173dc456185dc41da7ab2450c826b153768ae
Artur Morys - Magiera <artus9033@gmail.com>
2026-01-08 14:12:20 +0100

    feat: rework monorepo with Turborepo; update example apps and CI pipelines (#171)
    * chore: move RN CLI example app to dedicated folder
    
    * feat: bootstrap CLI & add Android commands
    
    * docs: added CLI docs
    
    * fix: update path to root project in metro and react-native config files
    
    * chore: upgraded pods in example app
    
    * feat: ios support PoC using Rock utilities
    
    * chore: bootstrap new RN app with RN CLI
    
    * feat: integrate brownfield into demo Android RN app
    
    * chore: bootstrap demo android app
    
    * feat: integrate brownfield RN lib into demo native Android app
    
    * ci: added CI for new Android app
    
    * docs: updated links to apps in docs
    
    * chore: bootstrap demo iOS app
    
    * feat: added brownfield framework target to RN iOS app
    
    * ci: added pipeline for new iOS app
    
    * feat: better logging in verbose mode in the CLI
    
    * feat: cleanup cli flags for CLI
    
    * chore: re-add brownfield target to RN iOS project
    
    * chore: use static frameworks in RN iOS app
    
    * fix: cli for iOS
    
    * chore: deintegrate old, obsolete example app
    
    * docs: added README in example/ directory
    
    * chore: static frameworks in RNApp
    
    * feat: pod install in CLI
    
    * feat: simplify iOS CLI logic, add xcframework merging
    
    * feat: integrate brownfield with demo iOS app
    
    * feat: new, nicer UI in demo Android app
    
    * ci: build Release variants
    
    * chore: remove obsolete tag in AndroidManifest.xml
    
    * chore: move cli sources and configuration after merge
    
    * chore: proper turborepo configuration
    
    * chore: provide brownfield bin
    
    * fix(ci): fix ci
    
    * fix: paths in demo RNApp configuration
    
    * fix: respect --no-install-pods CLI option
    
    * ci: merge workflows
    
    * ci: build iOS Release configuration
    
    * chore: remove yarn.lock in RNApp
    
    * chore: rename CI workflow, add badge to README.md
    
    * chore: move tester app, reorganize CI workflow
    
    * fix(ci): fix CLI commands in CI
    
    * fix(ci): use first available iOS simulator
    
    * fix(ci): right path to the CLI
    
    * fix(ci): use any iOS target
    
    * fix(ci): proper native app build commands
    
    * chore: add android / ios scripts to run the RN app
    
    * chore: rework turbo repo config
    
    * chore: add CONTRIBUTING.md and describe demo apps in docs
    
    * ci: use Turbo tasks instead of manual commands
    
    * ci: cache Turbo caches on GH Actions
    
    * fix: move build:brownfield task to turbo repo root
    
    * fix: iOS app script name
    
    * ci: build framework only for iphoneos to speed up
    
    * ci: build iOS artifact for iphonesimulator to match consumer app destination
    
    * chore: add linting of gradle plugins to the lint job
    
    * fix: add eslint dev-dependency to react-native-brownfield
    
    * chore: deintegrate CLI from this PR
    
    * ci: cache tester app in CI
    
    * chore: remove cli bin entry from yarn.lock
    
    * chore: rename tester-integrated directory to TesterIntegrated
    
    * chore: embed frameworks in Apple example app
    
    * fix(ci): fix caching keys
    
    * ci: rename caching steps
    
    * ci: cache gradle files
    
    * chore: enable ccache in Podfiles
    
    * chore: separate script for linting gradle plugin sources
    
    * ci: deintegrate iOS build caching that does not bring boost
    
    * chore: deintegrate CLI docs
    
    * ci: set concurrency rule to stop outdated running tasks
    
    * fix: typo in apps/README.md
    
    Co-authored-by: Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
    
    * chore: remove leftover CLI dependencies
    
    * ci: pin action commit checksums, validate gradle wrapper in android CI
    
    ---------
    
    Co-authored-by: Oskar Kwaśniewski <oskarkwasniewski@icloud.com>

---
+++ apps/README.md
@@ -0,0 +1,8 @@
+# React Native Brownfield demos
+
+This directory contains demo projects showcasing the usage of the `react-native-brownfield` library.
+
+- `RNApp` - the React Native application that is packaged to AAR and XCFramework archives and integrated into native projects
+- `AndroidApp` - the native Android application that integrates the RNApp AAR package (a "consumer" of the RNApp library)
+- `iOSApp` - the native iOS application that integrates the RNApp XCFramework package (a "consumer" of the RNApp library)
+- `TesterIntegrated` - a native tester application that integrates the native projects with and React Native project inside a single application, consuming the brownfield module directly, without packaging to an artifact; this is mostly useful for internal development purposes and is not as meaningful for end users of React Native Brownfield
---
+++ apps/RNApp/android/BrownfieldLib/build.gradle.kts
@@ -0,0 +1,128 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.callstack.react.brownfield")
+    `maven-publish`
+    id("com.facebook.react")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenAar") {
+            groupId = "com.rnapp"
+            artifactId = "brownfieldlib"
+            version = "0.0.1-local"
+            afterEvaluate {
+                from(components.getByName("default"))
+            }
+
+            pom {
+                withXml {
+                    /**
+                     * As a result of `from(components.getByName("default")` all of the project
+                     * dependencies are added to `pom.xml` file. We do not need the react-native
+                     * third party dependencies to be a part of it as we embed those dependencies.
+                     */
+                    val dependenciesNode =
+                        (asNode().get("dependencies") as groovy.util.NodeList).first() as groovy.util.Node
+                    dependenciesNode.children()
+                        .filterIsInstance<groovy.util.Node>()
+                        .filter { (it.get("groupId") as groovy.util.NodeList).text() == rootProject.name }
+                        .forEach { dependenciesNode.remove(it) }
+                }
+            }
+        }
+    }
+
+    repositories {
+        mavenLocal() // Publishes to the local Maven repository (~/.m2/repository by default)
+    }
+}
+
+val moduleBuildDir: Directory = layout.buildDirectory.get()
+
+/**
+ * As a result of `from(components.getByName("default")` all of the project
+ * dependencies are added to `module.json` file. We do not need the react-native
+ * third party dependencies to be a part of it as we embed those dependencies.
+ */
+tasks.register("removeDependenciesFromModuleFile") {
+    doLast {
+        file("$moduleBuildDir/publications/mavenAar/module.json").run {
+            val json = inputStream().use { JsonSlurper().parse(it) as Map<String, Any> }
+            (json["variants"] as? List<MutableMap<String, Any>>)?.forEach { variant ->
+                (variant["dependencies"] as? MutableList<Map<String, Any>>)?.removeAll { it["group"] == rootProject.name }
+            }
+            writer().use { it.write(JsonOutput.prettyPrint(JsonOutput.toJson(json))) }
+        }
+    }
+}
+
+tasks.named("generateMetadataFileForMavenAarPublication") {
+    finalizedBy("removeDependenciesFromModuleFile")
+}
+
+react {
+    autolinkLibrariesWithApp()
+}
+
+android {
+    namespace = "com.rnapp.brownfieldlib"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+
+        buildConfigField(
+            "boolean",
+            "IS_EDGE_TO_EDGE_ENABLED",
+            properties["edgeToEdgeEnabled"].toString()
+        )
+        buildConfigField(
+            "boolean",
+            "IS_NEW_ARCHITECTURE_ENABLED",
+            properties["newArchEnabled"].toString()
+        )
+        buildConfigField("boolean", "IS_HERMES_ENABLED", properties["hermesEnabled"].toString())
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    publishing {
+        multipleVariants {
+            allVariants()
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    api("com.facebook.react:react-android:0.82.1")
+    api("com.facebook.react:hermes-android:0.82.1")
+}
