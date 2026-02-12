a64e2fe93f54df4418ed676216d5d55c2c35d8d3
artus9033 <artus9033@gmail.com>
2026-02-12 12:08:41 +0100

    feat: reuse RNApp sources in ExpoApp

--- apps/ExpoApp/app/index.tsx
+++ apps/ExpoApp/app/index.tsx
@@ -1,15 +1,5 @@
-import { Text, View } from 'react-native';
+import App from '@callstack/brownfield-example-rn-app/src/App';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <App />;
 }
