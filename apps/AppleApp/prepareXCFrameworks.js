24db114b511348bdc4c7313392dab1614e30244a
artus9033 <artus9033@gmail.com>
2026-02-13 00:50:55 +0100

    feat: demo iOS app supporting interchanging brownfield artifacts

---
+++ apps/AppleApp/prepareXCFrameworks.js
@@ -0,0 +1,92 @@
+import path from 'node:path';
+import fs, { globSync } from 'node:fs';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// arg parser
+import yargs from 'yargs';
+
+const { appName } = yargs(process.argv.slice(2))
+  .usage('prepareXCFrameworks --appName <appName>')
+  .demandOption('appName', 'App name is required, pass it as an argument')
+  .parse();
+
+import { intro, outro, logger } from '@rock-js/tools';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+intro(`Preparing XCFrameworks for ${appName}`);
+
+if (!appName) {
+  throw new Error('App name is required, pass it as an argument');
+}
+
+const appPath = path.join(__dirname, '..', appName);
+
+if (!fs.existsSync(appPath)) {
+  throw new Error('Invalid app name, this app does not exist');
+}
+
+const sourcePackagePath = path.join(
+  appPath,
+  'ios',
+  '.brownfield',
+  'package',
+  'build'
+);
+
+const targetPackagePath = path.join(__dirname, 'package');
+
+if (fs.existsSync(targetPackagePath)) {
+  logger.info(`Removing ${targetPackagePath}\n`);
+  fs.rmSync(targetPackagePath, { recursive: true });
+}
+
+logger.info(`Copying ${sourcePackagePath} to ${targetPackagePath}\n`);
+fs.cpSync(sourcePackagePath, targetPackagePath, { recursive: true });
+
+/**
+ * The Xcode project is configured to link the following frameworks:
+ * - BrownfieldLib (constant)
+ * - Brownie (constant)
+ * - hermesvm <- this changes depending on RN version: for RN < 0.82 it's hermes.xcframework, for RN >= 0.82 it's hermesvm.xcframework
+ * - ReactBrownfield (constant)
+ *
+ * The trick is to rename the artifacts to match the Xcode project configuration.
+ */
+
+// handle hermesvm.xcframework / hermes.xcframework
+let hermesArtifactFound = false;
+for (const candidateDir of ['hermes.xcframework', 'hermesvm.xcframework']) {
+  if (fs.existsSync(path.join(targetPackagePath, candidateDir))) {
+    fs.renameSync(
+      path.join(targetPackagePath, candidateDir),
+      path.join(targetPackagePath, 'hermesvm.xcframework')
+    );
+    hermesArtifactFound = true;
+  }
+}
+
+if (!hermesArtifactFound) {
+  throw new Error('Hermes artifact not found');
+}
+
+// list files
+const validNames = [
+  'BrownfieldLib.xcframework',
+  'Brownie.xcframework',
+  'hermesvm.xcframework',
+  'ReactBrownfield.xcframework',
+];
+
+for (const file of fs.readdirSync(targetPackagePath)) {
+  if (!validNames.includes(file)) {
+    throw new Error(`Invalid file name: ${file}`);
+  }
+
+  logger.success(`${file} prepared`);
+}
+
+outro(`Done!`);
