881ee416479cd344e4d952306d36e88561d210df
Hur Ali <hurali97@gmail.com>
2026-02-17 16:45:02 +0500

    fix: bundle reference

--- packages/react-native-brownfield/src/expo-config-plugin/template/ios/ReactNativeHostManager.swift
+++ packages/react-native-brownfield/src/expo-config-plugin/template/ios/ReactNativeHostManager.swift
@@ -6,26 +6,26 @@ import UIKit
 
 public class ReactNativeHostManager {
   public static let shared = ReactNativeHostManager()
-
+  
   private var reactNativeDelegate: ExpoReactNativeFactoryDelegate?
   private var reactNativeFactory: RCTReactNativeFactory?
   private var expoDelegate: ExpoAppDelegate?
-
+  
   public func initialize() {
     let delegate = ReactNativeDelegate()
     let factory = ExpoReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
-
+    
     reactNativeDelegate = delegate
     reactNativeFactory = factory
-
+    
     expoDelegate = ExpoAppDelegate()
     expoDelegate?.bindReactNativeFactory(factory)
-
+    
     // required to avoid this being file be stripped by the swift compiler
     let _ = ExpoModulesProvider()
   }
-
+  
   // propagate delegate methods to ExpoAppDelegate
   public func application(
     _ application: UIApplication,
@@ -33,33 +33,32 @@ public class ReactNativeHostManager {
   ) -> Bool {
     ((expoDelegate?.application(application, didFinishLaunchingWithOptions: launchOptions)) != nil)
   }
-
+  
   // load & present RN UI
   public func loadView(
     moduleName: String, initialProps: [AnyHashable: Any]?,
     launchOptions: [AnyHashable: Any]?
   ) -> UIView {
+    let bundleURL = reactNativeDelegate?.bundleURL()
     return
-      (expoDelegate?.recreateRootView(
-        withBundleURL: reactNativeDelegate?.bundleURL(), moduleName: moduleName, initialProps: initialProps,
-        launchOptions: launchOptions))!
+    (expoDelegate?.recreateRootView(
+      withBundleURL: bundleURL, moduleName: moduleName, initialProps: initialProps,
+      launchOptions: launchOptions))!
   }
 }
 
 class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
-  // Extension point for config-plugins
-
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     // needed to return the correct URL for expo-dev-client.
     bridge.bundleURL ?? bundleURL()
   }
-
+  
   override func bundleURL() -> URL? {
-    #if DEBUG
-      return RCTBundleURLProvider.sharedSettings().jsBundleURL(
-        forBundleRoot: ".expo/.virtual-metro-entry")
-    #else
-      return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-    #endif
+#if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(
+      forBundleRoot: ".expo/.virtual-metro-entry")
+#else
+    return ReactNativeBundle.url(forResource: "main", withExtension: "jsbundle")
+#endif
   }
 }
