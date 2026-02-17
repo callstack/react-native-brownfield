52e18f28fe9775d9099181ff6186739375521c01
Hur Ali <hurali97@gmail.com>
2026-02-17 09:38:33 +0500

    chore: use latest version for brownfield-gradle-plugin

--- packages/react-native-brownfield/src/expo-config-plugin/android/constants.ts
+++ packages/react-native-brownfield/src/expo-config-plugin/android/constants.ts
@@ -1,2 +1,2 @@
-export const BROWNFIELD_PLUGIN_VERSION = '0.6.3';
+export const BROWNFIELD_PLUGIN_VERSION = '0.7.3';
 export const brownfieldGradlePluginDependency = `classpath("com.callstack.react:brownfield-gradle-plugin:${BROWNFIELD_PLUGIN_VERSION}")`;
