8a9e58f61c55fda9f9c2e5c90cae07b9a23ba8c1
Hur Ali <hurali97@gmail.com>
2026-02-17 14:42:27 +0500

    feat: add configuration for vanilla and expo to AppleApp

--- apps/AppleApp/Brownfield Apple App/BrownfieldAppleApp.swift
+++ apps/AppleApp/Brownfield Apple App/BrownfieldAppleApp.swift
@@ -1,6 +1,7 @@
 import BrownfieldLib
 import Brownie
 import SwiftUI
+import ReactBrownfield
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     var window: UIWindow?
@@ -9,19 +10,30 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
+#if USE_EXPO_HOST
         return ReactNativeHostManager.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+#else
+        return true
+#endif
     }
 }
 
 @main
 struct BrownfieldAppleApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+    
     init() {
+#if USE_EXPO_HOST
         ReactNativeHostManager.shared.initialize()
+#else
+        ReactNativeBrownfield.shared.bundle = ReactNativeBundle
+        ReactNativeBrownfield.shared.startReactNative {
+            print("React Native has been loaded")
+        }
+#endif
         BrownfieldStore.register(initialState)
     }
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
--- apps/AppleApp/Brownfield Apple App/ContentView.swift
+++ apps/AppleApp/Brownfield Apple App/ContentView.swift
@@ -1,10 +1,11 @@
+import ReactBrownfield
 import BrownfieldLib
 import Brownie
 import SwiftUI
 
 let initialState = BrownfieldStore(
-  counter: 0,
-  user: User(name: "Username")
+    counter: 0,
+    user: User(name: "Username")
 )
 
 struct ContentView: View {
@@ -16,23 +17,33 @@ struct ContentView: View {
     }
 }
 
+#if USE_EXPO_HOST
 struct RNViewRepresentable: UIViewRepresentable {
-
+    
     func makeUIView(context: Context) -> UIView {
         return ReactNativeHostManager.shared.loadView(moduleName: "RNApp", initialProps: nil, launchOptions: nil)
     }
-
+    
     func updateUIView(_ uiView: UIView, context: Context) {
         // Update the view when SwiftUI state changes
     }
 }
+#endif
 
 struct MainScreen: View {
+    var rnView: some View {
+#if USE_EXPO_HOST
+        RNViewRepresentable()
+#else
+        ReactNativeView(moduleName: "RNApp")
+#endif
+    }
+    
     var body: some View {
         VStack(spacing: 16) {
             GreetingCard(name: "iOS")
-
-            RNViewRepresentable()
+            
+            rnView
                 .navigationBarHidden(true)
                 .clipShape(RoundedRectangle(cornerRadius: 16))
                 .background(Color(UIColor.systemBackground))
@@ -45,19 +56,19 @@ struct MainScreen: View {
 struct GreetingCard: View {
     let name: String
     @UseStore(\BrownfieldStore.counter) var counter
-
+    
     var body: some View {
         VStack(spacing: 12) {
             Text("Hello native \(name) 👋")
                 .font(.title3)
                 .multilineTextAlignment(.center)
-
+            
             Text(
                 "You clicked the button \(Int(counter)) time\(counter == 1 ? "" : "s")"
             )
             .multilineTextAlignment(.center)
             .font(.body)
-
+            
             Button("Increment counter") {
                 $counter.set { $0 + 1 }
             }
