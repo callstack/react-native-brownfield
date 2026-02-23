f486ddbf79eb80627583f1deecdc35a64a0f6116
Hur Ali <hurali97@gmail.com>
2026-02-23 12:35:18 +0500

    fix(ios): remove brownie and react brownfield exported directives (#240)
    * fix(ios): remove brownie exported import
    
    * fix: remove exported directive

--- packages/react-native-brownfield/src/expo-config-plugin/template/ios/FrameworkInterface.swift
+++ packages/react-native-brownfield/src/expo-config-plugin/template/ios/FrameworkInterface.swift
@@ -1,6 +1,5 @@
-// Export helpers from @callstack/react-native-brownfield library
-@_exported import ReactBrownfield
-@_exported import Brownie
+import Foundation
+import ReactBrownfield
 
 // Initializes a Bundle instance that points at the framework target.
 public let ReactNativeBundle = Bundle(for: InternalClassForBundle.self)
