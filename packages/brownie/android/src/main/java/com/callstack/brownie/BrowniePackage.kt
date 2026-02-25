42d58456d2832693f3989e5f0f11729a9d5a71c6
Hur Ali <hurali97@gmail.com>
2026-02-25 13:48:40 +0500

    chore: scaffold android

---
+++ packages/brownie/android/src/main/java/com/callstack/brownie/BrowniePackage.kt
@@ -0,0 +1,21 @@
+package com.callstack.brownie
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+import java.util.Collections
+
+class BrowniePackage : ReactPackage {
+  override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
+    return Collections.emptyList()
+  }
+
+  override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+    val modules = ArrayList<NativeModule>()
+    modules.add(BrownieModule(reactContext))
+    return modules
+  }
+}
