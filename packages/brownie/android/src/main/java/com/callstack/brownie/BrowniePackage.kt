f78d9e16b660380770a0c4ce58d5c68643085319
Hur Ali <hurali97@gmail.com>
2026-02-26 11:29:15 +0500

    refactor: use new module registration API

--- packages/brownie/android/src/main/java/com/callstack/brownie/BrowniePackage.kt
+++ packages/brownie/android/src/main/java/com/callstack/brownie/BrowniePackage.kt
@@ -1,21 +1,31 @@
 package com.callstack.brownie
 
-import android.view.View
-import com.facebook.react.ReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ReactShadowNode
-import com.facebook.react.uimanager.ViewManager
-import java.util.Collections
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class BrowniePackage : ReactPackage {
-  override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
-    return Collections.emptyList()
+
+class BrowniePackage : BaseReactPackage() {
+  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+    return if (name == BrownieModule.NAME) {
+      BrownieModule(reactContext)
+    } else {
+      null
+    }
   }
 
-  override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
-    val modules = ArrayList<NativeModule>()
-    modules.add(BrownieModule(reactContext))
-    return modules
+  override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
+    mapOf(
+      BrownieModule.NAME to ReactModuleInfo(
+        name = BrownieModule.NAME,
+        className = BrownieModule.NAME,
+        canOverrideExistingModule = false,
+        needsEagerInit = false,
+        isCxxModule = false,
+        isTurboModule = true
+      )
+    )
   }
 }
