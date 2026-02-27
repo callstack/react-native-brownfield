c6484365b9bf5d27eecf34d435f473ea4b2eaa46
Hur Ali <hurali97@gmail.com>
2026-02-27 14:36:21 +0500

    refactor: remove unused style

--- apps/ExpoApp/components/counter/index.tsx
+++ apps/ExpoApp/components/counter/index.tsx
@@ -19,12 +19,6 @@ const Counter = () => {
 export default Counter;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
   text: {
     fontSize: 30,
     fontWeight: 'bold',
