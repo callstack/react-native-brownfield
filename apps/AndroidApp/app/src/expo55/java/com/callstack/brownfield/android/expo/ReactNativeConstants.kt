d45c59cf665026eacf124405f4aeab566c3e730c
artus9033 <artus9033@gmail.com>
2026-03-10 20:24:35 +0100

    feat: demo android expo 54 & 55 flavors

--- apps/AndroidApp/app/src/expo55/java/com/callstack/brownfield/android/expo/ReactNativeConstants.kt
+++ apps/AndroidApp/app/src/expo55/java/com/callstack/brownfield/android/expo/ReactNativeConstants.kt
@@ -2,5 +2,5 @@ package com.callstack.brownfield.android.example
 
 object ReactNativeConstants {
     const val MAIN_MODULE_NAME = "main"
-    const val APP_NAME = "Android (Expo)"
+    const val APP_NAME = "Android (Expo 55)"
 }
